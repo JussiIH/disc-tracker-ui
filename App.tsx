4cd92e69687466b2fda28c12cd10872f2c21aae0
Jussi H <jussi.hjerppe7@gmail.com>
2022-11-22 20:26:15 +0200

    discTracker-ui-init

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: 'red' }}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <StatusBar />
     </View>
-  ); 
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
